2b8154b4cd24bf501e27181145e811e0561201aa
Juan Bernardo Benavides <jbbenavidesr@unal.edu.co>
2021-08-01 20:21:53 -0500

    no need for Strapi, migrate to contentful

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
     // Set directories to pass through to the dist folder
     eleventyConfig.addPassthroughCopy("./src/images/");
+    eleventyConfig.addPassthroughCopy("./src/fonts/");
 
     eleventyConfig.addWatchTarget("./src/sass/");
 
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '7de8b945-05f5-425c-b9d1-9e873232cfc6'
-};
