a249df2348933e992f53f5f58c7d328774aa6679
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2021-08-21 10:42:41 -0500

    Blog section ready

--- src/_11ty/shortcodes/featuredBlogPostsSection.js
+++ src/_11ty/shortcodes/featuredBlogPostsSection.js
@@ -4,36 +4,40 @@ const client = contentful.createClient({
     accessToken: process.env.CTFL_ACCESSTOKEN,
 });
 
-
 module.exports = async function (featuredBlogPostsSection) {
     return await Promise.all(
-        featuredBlogPostsSection.fields.postList.map(function(post){
-            return client.getEntry(post.sys.id)
-            .then(function (response){
-                return `<article class="[ post ]">
+        featuredBlogPostsSection.fields.postList.map(function (post) {
+            return client
+                .getEntry(post.sys.id)
+                .then(function (response) {
+                    return `<article class="[ post ] [ flow ]">
                             <header>
 								<p class="post__theme">${response.fields.tema.fields.tema}</p>
 								<h3 class="post__title">${response.fields.title}</h3>
 							</header>
-							<section class="post__content">
+							<section class="[ post__content ] [ flow ]">
 								<p class="post__summary">${response.fields.resumen}</p>
 								<a href="/blog/${response.fields.slug}/" class="post__link">Leer más</a>
                         </article>`;
-            }).catch(function(error){
-                console.log(error);
-            })
-        })).then(function(cards){
-            return `<section class="[ bg-quinary-200 ]">
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        })
+    )
+        .then(function (cards) {
+            return `<section class="[ featured-blog-posts-section ] [ bg-quinary-200 ]">
+                        <header class="[ featured-blog-posts-section__heading ] [ align-center ]">
+                            <h2>${featuredBlogPostsSection.fields.title}</h2>
+                        </header>
                         <div class="wrapper flow">
-                            <header class="[ align-center ]">
-                                <h2>${featuredBlogPostsSection.fields.title}</h2>
-                            </header>
-                            <div class="[]">
+                            <div class="[ featured-blog-posts-section__body ]">
                                 ${cards.join("")}
                             </div>
                         </div>
                     </section>`;
-        }).catch(function (error){
+        })
+        .catch(function (error) {
             console.log(error);
         });
 };
